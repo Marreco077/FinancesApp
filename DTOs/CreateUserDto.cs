c988aac75944016a2b9bde72bb054e4c1fbf5150
Marcelo Lucas Pereira Filho <marcelolucas2103@gmail.com>
2026-06-13 12:26:03 -0300

    Updating validation rules

--- DTOs/CreateUserDto.cs
+++ DTOs/CreateUserDto.cs
@@ -1,8 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Finances.DTOs;
 
 public sealed record CreateUserDto(
+    [Required]
+    [StringLength(50, MinimumLength = 2)]
     string FirstName,
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
     string LastName,
+    [Required]
+    [StringLength(50, MinimumLength = 2)]
     string Nickname,
+    
+    [Required]
+    [StringLength(100)]
+    [EmailAddress]
     string Email,
+    
+    [Required]
+    [StringLength(255, MinimumLength = 8)]
     string Password);
--- DTOs/UpdateUserDto.cs
+++ DTOs/UpdateUserDto.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Finances.DTOs;
 
 public sealed record UpdateUserDto(
+    [StringLength(50, MinimumLength = 2)]
     string FirstName,
+    [StringLength(50, MinimumLength = 2)]
     string LastName,
+    [StringLength(50, MinimumLength = 2)]
     string Nickname,
+    [StringLength(100)]
+    [EmailAddress]
     string Email);
